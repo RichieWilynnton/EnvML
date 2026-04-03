dfadb071b7dbb96291bda0a3b8f8c98d114c80f2
jam-khan <jamkhan@connect.hku.hk>
2026-04-04 03:44:37 +0800

    removed lean-sce mechanization

--- lean-sce/LeanSce.lean
+++
@@ -1,9 +0,0 @@
--- This module serves as the root of the `LeanSce` library.
--- Import modules here that should be built as part of the library.
-import LeanSce.Core.Syntax
-import LeanSce.Core.Typing
-import LeanSce.Core.BigStep
-import LeanSce.Core.SmallStep
-import LeanSce.SCE.Semantics
-import LeanSce.SCE.Elaboration
-import LeanSce.SCE.Theories
